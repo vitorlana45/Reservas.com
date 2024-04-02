8b088221446753c543c9bb90de564556af8449a2
vitorlana45 <vitoresmerio00@gmail.com>
2024-04-01 22:19:01 -0300

    created the association of hotel and user entities

--- src/main/java/com/lanaVitor/Reservas/com/entities/Hotel.java
+++ src/main/java/com/lanaVitor/Reservas/com/entities/Hotel.java
@@ -5,13 +5,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "tb_hotels")
+@Table(name = "tb_hotel")
 public class Hotel {
 
     @Id
@@ -21,6 +23,12 @@ public class Hotel {
     private String Location;
     private String description;
 
+    @ManyToMany
+    @JoinTable(name = "tb_hotel_user",
+            joinColumns = @JoinColumn(name = "hotel_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private Set<User> users = new HashSet<>();
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -28,6 +36,7 @@ public class Hotel {
         Hotel hotel = (Hotel) o;
         return Objects.equals(id, hotel.id);
     }
+
     @Override
     public int hashCode() {
         return Objects.hash(id);

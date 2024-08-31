f0241cf4ed78db312ab0a1edd733ee6e0bac9e09
vitorlana45 <vitoresmerio00@gmail.com>
2024-08-31 15:34:16 -0300

    feat: added size to UserDTO fields

--- src/main/java/com/lanaVitor/Reservas/com/dtos/UserDTO.java
+++ src/main/java/com/lanaVitor/Reservas/com/dtos/UserDTO.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -18,12 +19,15 @@ public class UserDTO {
 
     private Long id;
     @Setter
+    @NotBlank(message = "O campo nome é obrigatório")
+    @Length(min = 3, max = 70, message = "O campo nome deve conter entre 3 e 100 caracteres")
     private String name;
     @Setter
     @Email(message = "O campo Email deve conter o formato correto")
     private String email;
 
-    @NotBlank
+    @NotBlank(message = "O campo senha é obrigatório")
+    @Length(min = 6, message = "A senha deve conter no mínimo 6 caracteres")
     private String password;
 
     @Getter

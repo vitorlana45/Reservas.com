d43567cbbe2b76d00908d8268edd8fc0f5f9fc9a
Vitor Lana <113393497+vitorlana45@users.noreply.github.com>
2024-12-19 21:36:47 -0300

    Update WebConfig.java
    changed front end url

--- src/main/java/com/lanaVitor/Reservas/com/config/WebConfig.java
+++ src/main/java/com/lanaVitor/Reservas/com/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "https://reservas-44eh.onrender.com", "https://reservas-five.vercel.app")
+                .allowedOrigins("http://localhost:4200", "https://reservas-44eh.onrender.com", "https://reservas-butfz4ppi-vitor-lanas-projects.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
